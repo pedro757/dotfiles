62fbdb0a48a54dc8b1297af0f871ebbb1c5df037
Pedro Sanchez <pedrosanchez757@hotmail.com>
2021-08-29 14:18:10 -0400

    feat: dap config debuggers

--- .config/nvim/lua/config/dap.lua
+++ .config/nvim/lua/config/dap.lua
@@ -1,6 +1,11 @@
 local dap_install = require("dap-install")
+local dbg_list = require("dap-install.debuggers_list").debuggers
 
 dap_install.setup({
 	installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
 	verbosely_call_debuggers = false,
 })
+
+for debugger, _ in pairs(dbg_list) do
+	dap_install.config(debugger, {})
+end
