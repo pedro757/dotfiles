fa2c0da8d31297874be3fabde015680b6c029db2
Pedro Sanchez <pedrosanchez757@hotmail.com>
2021-09-07 16:13:02 -0400

    feat: refactoring.nvim telescope integration

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -24,3 +24,30 @@ require('telescope').setup{
   },
 }
 require("telescope").load_extension("git_worktree")
+
+local refactor = require("refactoring")
+refactor.setup()
+
+-- telescope refactoring helper
+local function refactor(prompt_bufnr)
+    local content = require("telescope.actions.state").get_selected_entry(
+        prompt_bufnr
+    )
+    require("telescope.actions").close(prompt_bufnr)
+    require("refactoring").refactor(content.value)
+end
+
+M = {}
+
+M.refactors = function()
+    local opts = require("telescope.themes").get_cursor()
+    require("telescope.pickers").new(opts, {
+        prompt_title = "refactors",
+        finder = require("telescope.finders").new_table({
+            results = require("refactoring").get_refactors(),
+        }),
+        sorter = require("telescope.config").values.generic_sorter(opts),
+    }):find()
+end
+
+return M
