46a1ec3d475043e58dfeab65dd480540677a230f
Pedro Sanchez <pedrosanchez757@hotmail.com>
2021-09-08 18:31:42 -0400

    feat: dap config

--- .config/nvim/lua/config/dap.lua
+++ .config/nvim/lua/config/dap.lua
@@ -1,5 +1,6 @@
 local dap_install = require("dap-install")
 local dbg_list = require("dap-install.debuggers_list").debuggers
+local dbg_path = require("dap-install.debuggers_list").debuggers["jsnode_dbg"][2]
 
 dap_install.setup({
 	installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
@@ -7,5 +8,28 @@ dap_install.setup({
 })
 
 for debugger, _ in pairs(dbg_list) do
-	dap_install.config(debugger, {})
+	if debugger ~= 'jsnode_dbg' then
+		dap_install.config(debugger, {})
+	end
 end
+
+dap_install.config("jsnode_dbg", {
+	adapters = {
+		type = "executable",
+		command = "node",
+		args = {dbg_path .. "vscode-node-debug2/out/src/nodeDebug.js"}
+	},
+	configurations = {
+		{
+			type = "node2",
+			request = "launch",
+			program = "${workspaceFolder}/${file}",
+			cwd = vim.fn.getcwd(),
+			sourceMaps = true,
+			protocol = "inspector",
+			console = "integratedTerminal"
+		}
+	}
+})
+
+require("dapui").setup()
