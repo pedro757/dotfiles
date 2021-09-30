44a6a1be4322b20794f4c3f6b44be181cb36e69f
Pedro Sanchez <pedrosanchez757@hotmail.com>
2021-09-29 20:07:39 -0400

    fix: unused dap config

--- .config/nvim/lua/config/dap.lua
+++ .config/nvim/lua/config/dap.lua
@@ -1,7 +1,6 @@
 local dap_install = require("dap-install")
 local di_api = require("dap-install.api.debuggers")
 local dbg_list = di_api.get_installed_debuggers()
-local dbg_path = require("dap-install.config.settings").options["installation_path"]
 
 dap_install.setup({
 	installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
@@ -9,28 +8,7 @@ dap_install.setup({
 })
 
 for _, debugger in ipairs(dbg_list) do
-	if debugger ~= 'jsnode' then
-		dap_install.config(debugger, {})
-	end
+	dap_install.config(debugger, {})
 end
-
-dap_install.config("jsnode", {
-	adapters = {
-		type = "executable",
-		command = "node",
-		args = { dbg_path .. "jsnode/" .. "vscode-node-debug2/out/src/nodeDebug.js" }
-	},
-	configurations = {
-		{
-			type = "node2",
-			request = "launch",
-			program = "${workspaceFolder}/${file}",
-			cwd = vim.fn.getcwd(),
-			sourceMaps = true,
-			protocol = "inspector",
-			console = "integratedTerminal"
-		}
-	}
-})
 
 require("dapui").setup()
