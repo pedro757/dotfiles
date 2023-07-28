1e7e63b997021f3d9a9fed8ab405ecac84a16fb4
Pedro Sanchez <pedro.sanchez.jozic@gmail.com>
2023-07-27 21:41:12 -0400

    update

--- .config/nvim/lua/config/variables.lua
+++ .config/nvim/lua/config/variables.lua
@@ -29,3 +29,16 @@ if vim.loop.cwd() == "/home/pedro/.config/nvim" then
   vim.env.GIT_DIR = vim.fn.expand("~") .. "/.dotfiles/"
   vim.env.GIT_WORK_TREE = vim.fn.expand("~")
 end
+
+vim.g.clipboard = {
+ name = "xsel",
+ copy = {
+  ["+"] = "xsel --nodetach -i -b",
+  ["*"] = "xsel --nodetach -i -p",
+ },
+ paste = {
+  ["+"] = "xsel  -o -b",
+  ["*"] = "xsel  -o -b",
+ },
+ cache_enabled = 1,
+}
