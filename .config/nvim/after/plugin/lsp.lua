de6331d2e09a42f83e5831e062a081ebb106f8ec
Pedro Sanchez <pedro.sanchez.jozic@gmail.com>
2023-08-08 23:32:27 -0400

    update lua_ls

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -264,40 +264,31 @@ nvim_lsp.gopls.setup {
 
 require("neodev").setup {}
 
-local lua_ls_root_path = "/home/pedro/Documents/lua-language-server"
-local lua_ls_binary = lua_ls_root_path .. "/bin/lua-language-server"
-
 local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 nvim_lsp.lua_ls.setup {
-  cmd = { lua_ls_binary },
-  settings = {
-    Lua = {
-      runtime = {
-        version = "LuaJIT",
-        path = runtime_path,
-      },
-      diagnostics = {
-        enable = false,
-        globals = { "vim" },
-      },
-      completion = {
-        keywordSnippet = "Replace",
-        callSnippet = "Replace",
-      },
-      workspace = {
-        -- preloadFileSize = 145,
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-  on_attach = on_attach,
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT'
+        },
+        -- Make the server aware of Neovim runtime files
+        workspace = {
+          library = { vim.env.VIMRUNTIME }
+          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+          -- library = vim.api.nvim_get_runtime_file("", true)
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
 }
 
 vim.diagnostic.config {
