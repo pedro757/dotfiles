01958b5a68af63335e4e8280faf284e4b308a2bf
Pedro Sanchez <pedro.sanchez.jozic@gmail.com>
2025-06-19 10:28:41 -0400

    Add mcphub

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -244,12 +244,32 @@ local plugins = {
           enabled = true,
           provider = "mini_diff",
         },
+      },
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true,  -- Show mcp tool results in chat
+            make_vars = true,            -- Convert resources to #variables
+            make_slash_commands = true,  -- Add prompts as /slash commands
+          }
+        }
       }
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+  },
+  {
+      "ravitemer/mcphub.nvim",
+      dependencies = {
+          "nvim-lua/plenary.nvim",
+      },
+      build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+      config = function()
+          require("mcphub").setup()
+      end
   },
   {
     'echasnovski/mini.ai',
