43378ed9fe4c949fd11a248a5664fc3a05781ca5
Pedro Sanchez <pedro.sanchez.jozic@gmail.com>
2025-06-19 10:45:23 -0400

    Cormat

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -262,14 +262,14 @@ local plugins = {
     },
   },
   {
-      "ravitemer/mcphub.nvim",
-      dependencies = {
-          "nvim-lua/plenary.nvim",
-      },
-      build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
-      config = function()
-          require("mcphub").setup()
-      end
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+    config = function()
+        require("mcphub").setup()
+    end
   },
   {
     'echasnovski/mini.ai',
